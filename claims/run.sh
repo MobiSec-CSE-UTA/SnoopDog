bbc28c0519cf4a777e97655e5f1e1a330db69296
youngtakcho1988 <120425532+youngtakcho1988@users.noreply.github.com>
2025-09-12 20:53:32 -0500

    Update run.sh

--- claims/run.sh
+++ claims/run.sh
@@ -1,16 +1,17 @@
 !/bin/bash
+
 echo 'For benign device 1'
 echo 'target data file to load = ./recorded_data/benign1.data'
-python snoopdog_detector.py --data ./recorded_data/benign1.data
+python3 snoopdog_detector.py --data ./recorded_data/benign1.data
 
 echo 'For benign device 2'
 echo 'target data file to load = ./recorded_data/benign2.data'
-python snoopdog_detector.py --data ./recorded_data/benign2.data
+python3 snoopdog_detector.py --data ./recorded_data/benign2.data
 
 echo 'For malicious sniffer 1'
 echo 'target data file to load = ./recorded_data/malicious1.data'
-python snoopdog_detector.py --data ./recorded_data/malicious1.data
+python3 snoopdog_detector.py --data ./recorded_data/malicious1.data
 
 echo 'For malicious sniffer 2'
 echo 'target data file to load = ./recorded_data/malicious2.data'
-python snoopdog_detector.py --data ./recorded_data/malicious2.data
+python3 snoopdog_detector.py --data ./recorded_data/malicious2.data
